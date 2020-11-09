d9132dce714e0723858c6932fe786a7cc824812b
Ivan Alenin <aiscorp@yandex.ru>
2020-11-10 01:23:01 +0300

    GraphQl begin

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -2,7 +2,7 @@
 if (process.env.NODE_ENV === 'development') {
   window.ENV = {
     baseUrl: '',
-    serverUrl: 'http://192.168.0.159:5000/'
+    serverUrl: 'https://ais-code.ru/'
   }
 
 } else if (process.env.NODE_ENV === 'production') {
